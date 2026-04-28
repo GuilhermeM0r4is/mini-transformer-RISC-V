dd79c867ee0f39706dcd27c5512335ad72ba4151
Guilherme Morais <guilherme.m.morais@tecnico.ulisboa.pt>
2026-04-28 12:45:49 +0100

    Update argmax.s

--- argmax.s
+++ argmax.s
@@ -27,15 +27,18 @@ exit:
 argmax:
     blez a2, error   # Case where the number of elements is 0 or less (invalid)
     lw t0, 0(a1)   # Register containing the maximum value
+    
     li t1, 0   # Register containing the index of the maximum value
     li t2, 1   # Register containing the loop counter's value
     
 looping:
     beq t2, a2, looped   # If true, all elements have already been tested
+    
     slli t3, t2, 2   # Calculate the offset to reach the next element (mult by 4)
     add t4, t3, a1   # Adds the offset to the pointer's initial position
     lw t5, 0(t4)   # Places the new element to compare inside t5
     ble t5,t0, reloop   # If new element is inferior or equal to current maximum, stop this loop
+    
     mv t0, t5   # Updates the maximum value to the one contained in t5 (new maximum)
     mv t1, t2   # Updates the contained index to that of the current maximum
     
