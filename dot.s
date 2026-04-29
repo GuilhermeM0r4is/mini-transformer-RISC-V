acf90a5b2a2574326a81079333cf3b6ede55445c
Guilherme Morais <guilherme.m.morais@tecnico.ulisboa.pt>
2026-04-29 11:14:17 +0100

    Dot function (apenas para teste e comparação)
    Isto foi só uma que eu fiz para teste, para comparar com as vossas depois, e vermos as melhores.

--- dot.s
+++ dot.s
@@ -27,7 +27,40 @@ exit:
 #   a1 = dot product result
 # ===========================================================================
 dot:
-  # TODO: Implement the dot product function here
+    li t0, 1
+    blt a3, t0, invalid_size    # invalid size a3 < 1
+    li t0, 0    # sums of all the values, used to sum up different mults
+
+    slli t1, a3, 2    # mover os 4 bits necessários para ver o último valor
+    add t1, t1, a1    # limite total que podemos ter
+    
+dot_cicle:
+    lw t2, 0(a1)    # loads the first value of the first vector
+    lw t3, 0(a2)    # loads the first value of the second vector
+    
+    mulh t4, t2, t3      # since mul consists on a 64 bit value, when you use mulh
+    bnez t4, overflow    # it gets the value to 32 highest bits and 32 lowest bits
+                         # if the highest bits are different than 0, then there was
+                         # an overflow, as values can only store up to 32 bits      
+    
+    mul t4, t2, t3    # t6 = t2 * t3
+    add t0, t0, t4    # t0 += t2 * t3
+    
+    addi a1, a1, 4    # moves to the next value in the vector
+    addi a2, a2, 4    # moves to the next value in the vector
+    
+    bne a1, t1, dot_cicle    # if not the same, the cicle repeats
+    j dot_end    # returns to the end when the cicle ends
+
+invalid_size:
+    li a0, 50    # invalid size < 1
+    ret
+    
+overflow:
+    li a0, 200    # bytes overflow
+    ret
 
 dot_end:
+  li a0, 0    # all succeded until here
+  mv a1, t0    # gives a1 the dot product result
   jr ra               # return to the caller
