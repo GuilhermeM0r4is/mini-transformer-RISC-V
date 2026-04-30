a39bc60a3e6e635c798604ab17e66a7e47e2d287
Guilherme Morais <guilherme.m.morais@tecnico.ulisboa.pt>
2026-04-30 10:53:38 +0100

    Dot update - overflow done
    Já tenho o overflow error a funcionar

--- dot.s
+++ dot.s
@@ -38,13 +38,34 @@ dot_cicle:
     lw t2, 0(a1)    # loads the first value of the first vector
     lw t3, 0(a2)    # loads the first value of the second vector
     
-    mulh t4, t2, t3      # since mul consists on a 64 bit value, when you use mulh
-    bnez t4, overflow    # it gets the value to 32 highest bits and 32 lowest bits
-                         # if the highest bits are different than 0, then there was
-                         # an overflow, as values can only store up to 32 bits      
+    mul t4, t2, t3    # t4 = t2 * t3 
+        
+                       # since mul consists on a 64 bit value, when you use mulh
+    srai t5, t4, 31    # it gets the value to 32 highest bits and 32 lowest bits
+                       # if the highest bits are different than 0, then there was
+    mulh t6, t2, t3    # an overflow when multiplying two positive numbers, as 
+                       # values can only store up to 32 bits. However, when you use
+                       # negative numbers, this won't work exactly like that, so we
+                       # use the shift to move the values to the other side and now
+                       # we can check if their values are not equal, if not, then we
+                       # have an overflow problem
+                       
+    bne t5, t6, overflow_error    # t5 != t4, then error, as explained above
     
-    mul t4, t2, t3    # t6 = t2 * t3
-    add t0, t0, t4    # t0 += t2 * t3
+    add t5, t0, t4    # t0 += t2 * t3
+    
+    xor t6, t0, t4    # xor compares the different values of the numbers, if its
+                      # both positive or both negative, returns 0, in that case
+                      # there can be overflow, if both are different -> no overflow
+    
+    bltz t6, move_pointers    # it was zero, so there can be an overflow
+    
+sum_overflow_check:   
+    xor t6, t0, t5    # compares the number values again
+    bltz t6, overflow_error    # the numbers have different signs
+    
+move_pointers:
+    mv t0, t5    # gives the sum value to t0, to continue the acumulator logic
     
     addi a1, a1, 4    # moves to the next value in the vector
     addi a2, a2, 4    # moves to the next value in the vector
@@ -56,7 +77,7 @@ invalid_size:
     li a0, 50    # invalid size < 1
     ret
     
-overflow:
+overflow_error:
     li a0, 200    # bytes overflow
     ret
 
