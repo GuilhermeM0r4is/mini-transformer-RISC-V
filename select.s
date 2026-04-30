c8d8935177a0e477c058c6fd4861c0eb66590001
Guilherme Morais <guilherme.m.morais@tecnico.ulisboa.pt>
2026-04-30 10:59:34 +0100

    Select Update - Gui Otimizado

--- select.s
+++ select.s
@@ -1,4 +1,3 @@
-# You can change these values to test your solution.
 .data
 ARRAY: .word -6 -1 6 1
 SIZE:  .word 4
@@ -14,40 +13,41 @@ exit:
   li a7, 10         # exit syscall code
   ecall             # terminate the program
 
-# ==========================================================================
-# FUNCTION: select
-#   This function selects an element from an integer array.
-# Arguments:
-#   a1 = pointer to int array
-#   a2 = array length
-#   a3 = element index
-# Returns:
-#   a0 = status code
-#   a1 = value of the selected element
-# ===========================================================================
+#==========================================================================
+#FUNCTION: select
+  #This function selects an element from an integer array.
+Arguments:
+  #a1 = pointer to int array
+  #a2 = array length
+  #a3 = element index
+Returns:
+  #a0 = status code
+  #a1 = value of the selected element
+#===========================================================================
 select:
-    li x5, 1
-    blt a2, x5, invalid_arg    # if size <1 jumps to error code
+    li t0, 1    # Loads value 1 to t0
+    blt a2, t0, erro50    # if a2 < t0 function erro50 is called
+
+    blt a3, x0, erro100    # if a < 0 function erro100 is called
+    bge a3, a2, erro100    # the index cant be >= than the array size
+
+    # an int occupies 4 bytes, therefore, in order to change position,
+    # it is required to multiply the index by 4
     
-    li x5, 0
-    blt a3, x5, idx_out_of_range    # if negative index
-                                    # OR
-    bge a3, a2, idx_out_of_range    # if a3 >= a2
-    
-    slli x5, a3, 2   # x5 = a3 * 4, multiplies by 4 bytes
-    add a1, a1, x5    # adds to the return to get the exact position
-    lw a1, 0(a1)    # loads the local value of that index
-    
-    li a0, 0    # succeded -> code 0
+    slli t1, a3, 2    # t1 = a3 × 4
+    add  t1, a1, t1    # t1 = a1 + skipped bytes -> adress of the index element
+    lw   a1, 0(t1)    # loads the value stored in that adress to a1
+    li   a0, 0    # sucess
+
     ret
-    
-invalid_arg:    # garante que se tamanho < 1 temos um erro
-    li a0, 50    # code 50
-    ret
-    
-idx_out_of_range:    # makes sure the index is not out of bounds
-    li a0, 100    # code 100
+
+erro50:
+    li a0,50    # Loads 50 value to a0
+    ret 
+
+erro100:
+    li a0,100
     ret
 
 select_end:
-    jr ra    # return to the caller
+  jr ra               # return to the caller
